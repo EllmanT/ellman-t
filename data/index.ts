cfac22b971de5960da7d0e404a2c1d5cae45642d
EllmanT <tmuranda1@gmail.com>
2025-07-02 12:31:07 +0200

    first commit

---
+++ data/index.ts
@@ -0,0 +1,215 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I  create solutions clients ACTUALLY use.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "It doesnt matter where you are.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Technologies",
+    description: "I keep up with the latest",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 6,
+    title: "Do you want to work with me?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Building AI Agents with Inngest, Anthropic and Convex",
+    description: "Latest 📢",
+    className: "md:col-span-3 md:row-span-2 ",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 4,
+    title: "A tech lover.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "FiscalLens",
+    des: "A REAL Software-as-a-Service app with AI features that allows users to process and verify tax documents.",
+    img: "/fiscalens2.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/c.svg", "/claude.png"],
+    link: "https://docs-v2-murex.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Mojito",
+    des: "Expressive design piece that is brought to live using the latest GSAP technologies.",
+    img: "/moji.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://gsap-cocktails-main.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Devflow",
+    des: "A modern stackoverflow clone with a sleek design and a touch of AI.",
+    img: "/devflow2.png",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/next.svg",
+      "/fm.svg",
+      "/openai.webp",
+    ],
+    link: "https://jsm-devflow-gamma.vercel.app/",
+  },
+
+  {
+    id: 4,
+    title: "AgentX",
+    des: "Your own personal assistant that uses tools to read YT videos, access database, WIkipedia and many more and share incredible insights..",
+    img: "/agentx2.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/c.svg", "/cx.webp"],
+    link: "https://yt-ai-agent-neon.vercel.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Tapiwa was an eye-opener to the future of automation. His innovative thinking and ability to implement AI tools at Axis Solutions made a measurable impact. He doesn't just follow trends — he creates them. Truly a valuable asset to any tech-driven organization.",
+    name: "",
+    title: "",
+  },
+  {
+    quote:
+      "Tapiwa is a powerhouse — sharp, articulate, and driven. I’ve watched him speak with clarity and conviction, and his work ethic is second to none. His ability to lead conversations around AI and digital transformation is both inspiring and impactful.",
+    name: "",
+    title: "",
+  },
+  {
+    quote:
+      "What stood out with Tapiwa was his attention to detail and genuine care for getting things right. As a reseller, I’ve worked with many tech people, but Tapiwa took the time to understand my needs and delivered a solution that matched my exact specs — only then did he even mention cost. That kind of integrity is rare.",
+    name: "",
+    title: "",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "nextJS",
+    img: "/nextjs.svg",
+    nameImg: "/nextt.png",
+  },
+  {
+    id: 2,
+    name: "AI",
+    img: "/aai.svg",
+    nameImg: "/aai.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "Vercel",
+    img: "/vercel.svg",
+    nameImg: "/vercel.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Full Stack Dev Intern -Axis Solutions",
+    desc: "Assisted in the development of a web-based platform using Angular.js and PHP, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Full Stack Dev (AI) - Axis Solutions",
+    desc: "Designed and developed automation tools using GCP, Selenium, AngularJs, Nodejs and Docker",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "JS Mastery Courses",
+    desc: "Certified by JS Mastery as a full stack NextJs Developer. Participated in 6 month course levelling up my Dev Skills",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Certified Data Analyst",
+    desc: "Completed 4 months gaining experience with data analysis, big data Python R and just understanding the value of data.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/EllmanT",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/tapiwa-muranda-85747b335/",
+  },
+];
